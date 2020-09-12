cd72da70f96357de01196b498b6757df0333786d
rifqialfaizi <rifqialfaizii@gmail.com>
2020-09-12 09:24:33 +0700

    Initial Commit

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Rifqi Alfaizi on 12/09/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
