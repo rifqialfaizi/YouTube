e26f370affed15eaadc34d0e31ab3f591ca82ab9
rifqialfaizi <rifqialfaizii@gmail.com>
2021-10-18 20:43:08 +0700

    add embed static for video playlist

--- YouTube/Helper/Constants.swift
+++ YouTube/Helper/Constants.swift
@@ -14,4 +14,5 @@ struct Constants {
     static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
 }
